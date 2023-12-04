d98cbdb0be3050e5328b16361f5be48c41c19b30
JuanCompaire <juancompaire2004@gmail.com>
2023-12-04 10:16:37 +0100

    prueba

---
+++ pokemonexamen/src/test/java/com/example/pokemonexamen/PokemonexamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemonexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonexamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
