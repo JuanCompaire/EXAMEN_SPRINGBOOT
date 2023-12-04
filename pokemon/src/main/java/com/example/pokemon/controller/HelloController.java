4edf294c455f6116a234430985273101f49ebf55
JuanCompaire <juancompaire2004@gmail.com>
2023-12-04 10:01:08 +0100

    no me sale el index, y ns porque

--- pokemon/src/main/java/com/example/pokemon/controller/HelloController.java
+++ pokemon/src/main/java/com/example/pokemon/controller/HelloController.java
@@ -1,7 +1,47 @@
 package com.example.pokemon.controller;
 
+import com.example.pokemon.model.Pokemon;
+import com.example.pokemon.model.Region;
+import com.example.pokemon.service.PokemonService;
+import com.example.pokemon.service.RegionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class HelloController {
+
+    @Autowired
+    PokemonService pokemonService;
+
+    @Autowired
+    RegionService regionService;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        cargarListas(model);
+        return "index";
+    }
+
+    @RequestMapping("/finRegion")
+    public String finRegion(Model model){
+        cargarListas(model);
+        return "finRegion";
+    }
+
+    @RequestMapping("/finPokemon")
+    public String finPokemon(Model model){
+        cargarListas(model);
+        return "finPokemon";
+    }
+
+    private void cargarListas(Model model){
+        List<Pokemon> listaPokemons = pokemonService.listarTodosPokemon();
+        model.addAttribute("pokemons", listaPokemons);
+        List<Region> listaRegiones = regionService.listarTodosRegion();
+        model.addAttribute("regiones", listaRegiones);
+    }
 }
